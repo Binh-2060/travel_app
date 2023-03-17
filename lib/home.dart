b25bdd4f2b2d6b87da29ac138f03c3b87a81147f
Binh-2060 <binhxayxana@gmail.com>
2023-03-18 00:03:37 +0700

    Initial commit

---
+++ lib/home.dart
@@ -0,0 +1,65 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: CachedNetworkImageProvider(
+                        'https://images.unsplash.com/photo-1551918120-9739cb430c6d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80'),
+                    fit: BoxFit.cover)),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: const Text(
+                  'Explore and discover new places',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      fontSize: 32,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w500),
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: const Text(
+                  'Browse a lot of interesting tourist offers and choose something for yourself. The world is waiting!',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(color: Colors.white, fontSize: 16),
+                ),
+              ),
+              Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 25, vertical: 10),
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Block Button'),
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 48),
+                    primary: Colors.blue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,3 @@
+import 'package:travel_app/home.dart';
+
+var routes = {'/': (context) => const HomeScreen()};
