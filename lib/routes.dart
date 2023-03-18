038d8ebe0d363cdea93a881f122fa3955b1cb9e2
I-am-Bubu <Thiptida58@gmail.com>
2023-03-18 09:15:47 +0700

    explore-Page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,7 @@
+import 'package:travel_app/explore.dart';
 import 'package:travel_app/home.dart';
 
-var routes = {'/': (context) => const HomeScreen()};
+var routes = {
+  '/': (context) => const HomeScreen(),
+  '/explore': (context) => const ExplorePage()
+};
